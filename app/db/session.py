f31f838e9d5a7930560e7abf5d243befa6b5d147
Kirill <karachevkir@yandex.ru>
2026-02-09 23:22:16 +0300

    was added .env

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from app.core.config import settings
 
 
 async_engine = create_async_engine(
-    "postgresql+asyncpg://postgres:ZZZclash19963@localhost:5432/notes"
+    settings.DATABASE_URL,
+    echo=True
 )
 
 new_session = async_sessionmaker(async_engine, expire_on_commit=False)
 engine = create_async_engine(
-    "postgresql+asyncpg://postgres:ZZZclash19963@localhost:5432/notes",
-    echo=True,
+    settings.DATABASE_URL,
+    echo=True
 )
